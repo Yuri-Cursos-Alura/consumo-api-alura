ce6b3c400524e96d99fc0480c0f086f9dd25286d
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-04-11 16:42:55 -0300

    Add project files.

---
+++ Models/Music.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace consumo_api_alura.Models;
+internal class Music
+{
+    [JsonPropertyName("song")]
+    public string? Name { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int? DurationMs { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genre { get; set; }
+
+    [JsonIgnore]
+    public TimeSpan Duration => IsValid ? new TimeSpan(0, 0, 0, 0, 0, microseconds: DurationMs!.Value) : new TimeSpan(0);
+
+    [JsonIgnore]
+    public bool IsValid
+    {
+        get
+        {
+            if (Name is null) return false;
+            if (Artist is null) return false;
+            if (DurationMs is null) return false;
+            if (Genre is null) return false;
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using consumo_api_alura.Models;
+using System.Net;
+using System.Text.Json;
+
+var endpoint = "https://guilhermeonrails.github.io/api-csharp-songs/songs.json";
+
+
+using (HttpClient client = new())
+{
+    var httpResponse = await client.GetAsync(endpoint);
+
+    string? response = httpResponse.IsSuccessStatusCode ? await httpResponse.Content.ReadAsStringAsync() : null;
+    
+    if (response is null)
+    {
+        Console.WriteLine("Unable to get enpoint.");
+        return;
+    }
+
+    var musicas = JsonSerializer.Deserialize<List<Music>>(response);
+
+    if (musicas is null)
+    {
+        Console.WriteLine("Was not able to deserialize to List<Music>.");
+        return;
+    }
+
+    foreach (var musica in musicas)
+    {
+        Console.WriteLine($"- {musica.Name} | IsValid: {musica.IsValid}");
+    }
+}
